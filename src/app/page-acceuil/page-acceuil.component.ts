a17d9ca9839fc19a5f0805cc9ed60804b2e2bdd1
saousanebst <bastaoui.saousane@gmail.com>
2024-05-30 16:08:55 +0200

    firstcommit

---
+++ src/app/page-acceuil/page-acceuil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-acceuil',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-acceuil.component.html',
+  styleUrl: './page-acceuil.component.css'
+})
+export class PageAcceuilComponent {
+
+}
